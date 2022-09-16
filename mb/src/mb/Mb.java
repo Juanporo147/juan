d7e0895f72032e105cf7ac1d76ca0ff6bbb9eb88
Juanporo147 <113706339+Juanporo147@users.noreply.github.com>
2022-09-16 10:12:01 -0500

    Add files via upload

---
+++ mb/src/mb/Mb.java
@@ -0,0 +1,24 @@
+/**
+ * Nombres:Kevin Gomez, Ricardo Sevilla, Nayeli Parra
+ * Curso:3ro Inf A
+ * Tema:Proyecto mb
+ */
+package mb;
+import java.util.Scanner;
+public class Mb {
+
+
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        int opcion = 0,cant;
+        System.out.println("1.Case ensamblados");
+        System.out.println("2.Mouse ");
+        
+        switch(opcion){
+            case 1:
+                
+        }//fin switch
+        
+    }//fin main
+    
+}//fin class
